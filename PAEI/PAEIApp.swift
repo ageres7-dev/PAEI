bee69d3fbde0f6e7426ac82bec526ea4473c3b7b
ageres7-dev <66694244+ageres7-dev@users.noreply.github.com>
2021-01-02 00:50:22 +0500

    Initial Commit

---
+++ PAEI/PAEIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PAEIApp.swift
+//  PAEI
+//
+//  Created by Сергей on 02.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct PAEIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
